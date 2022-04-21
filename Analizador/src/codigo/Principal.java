29b61528adef47880f571781787805b1ac330380
dFungm <66537551+dFungm@users.noreply.github.com>
2022-04-20 22:18:05 -0600

    analizador
    (exer, lexerCup, Principal and Tokens added

---
+++ Analizador/src/codigo/Principal.java
@@ -0,0 +1,9 @@
+package codigo;
+
+public class Principal {
+    public static void main(String[] args) {
+        String path = "C:/Users/Diego/OneDrive - Estudiantes ITCR/Documentos/GitHub/Gramatica-bnf/Analizador";
+
+    }
+
+}
